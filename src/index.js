4551e6303264882085548401eedd605b6b242919
Talha <talha.b.future@gmail.com>
2022-03-13 11:29:11 +0300

    the js works now , changed styles location

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
+
 import _ from 'lodash';
-import './style.css';
+// import './style.css';
 
-console.log(`your computer has virus`)
-console.log(` @@ new watch menue`)
+ function component() {
+   const element = document.createElement('div');
 
-// const hehe = document.getElementById('hehe');
-// hehe.classList.add('testing')
+   // Lodash, now imported by this script
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+   return element;
+ }
+
+ document.body.appendChild(component());                    
+ 
 
 
-console.log(`444444444444444444444444444`)
+ 
+ // import _ from 'lodash';
+// import './style.css'
+
+// // console.log(`your computer has virus`)
+// console.log(` @@ new watch menue`)
+
+// const hehe = document.getElementById("hehe");
+
+// // const divHeader = getElementById('header');
+// // const aha = document.createElement('p')
+// aha.classList.add('testing')
+
+
+// hehe.classList.add(`testing`)
+// // hehe.textContent = `AHAHAH`
+
+// divHeader.appendChild(aha)
+
+
+// console.log(`444444444444444444444444444`)
 
