3febc0ee8a7a3c5c7ca669a6cc31f845e7bfa7c9
Talha <talha.b.future@gmail.com>
2022-03-14 11:33:42 +0300

    seperated food functions added grid style add img

--- src/index.js
+++ src/index.js
@@ -14,7 +14,6 @@ import _ from 'lodash';
 
    return element;
  }
-
  document.body.appendChild(component());                    
  
 
@@ -22,30 +21,8 @@ import _ from 'lodash';
 function menu() {
   //the main div 
   const mainDiv = document.createElement('div');
+   mainDiv.classList.add(`mainDiv`)
   
-  const div1 = document.createElement('div');
-  const Par1 = document.createElement('p');
-  Par1.textContent = `here is a text, above it a food pic`;
-  
-  const image1 = document.createElement('img');
-  image1.classList.add(`foodImg`)
-  image1.src = "img/img1.jpg"
-
-  div1.appendChild(image1)
-  div1.appendChild(Par1)
- 
-
-
-
-  const div2 = document.createElement('div');
-  const Par2 = document.createElement('p');
-  Par2.textContent = `second para`;
-  div2.appendChild(Par2)
-
-  const div3 = document.createElement('div');
-  const Par3 = document.createElement('p');
-  Par3.textContent = `third perhaps ?`;
-  div3.appendChild(Par3)
 
 
   const div4 = document.createElement('div');
@@ -55,13 +32,88 @@ function menu() {
 
 
 
-  mainDiv.appendChild(div1)
-  mainDiv.appendChild(div2)
-  mainDiv.appendChild(div3)
-  mainDiv.appendChild(div4)
+  mainDiv.appendChild(food1()) 
+  mainDiv.appendChild(food2())
+  mainDiv.appendChild(food3())
+  mainDiv.appendChild(food4())
   return mainDiv;
 
 }
+
+//this will be testing for a cleaner code, putting the foods in a seperate functions 
+function food1() {
+  const div1 = document.createElement('div');
+  const Par1 = document.createElement('p');
+  Par1.textContent = `here is a text, above it a food picffffffffffffffffffff`;
+  Par1.classList.add(`foodText`)
+
+  const image1 = document.createElement('img');
+  image1.classList.add(`foodImg`)
+  image1.src = "img/img1.jpg"
+
+  div1.appendChild(image1)
+  div1.appendChild(Par1)
+ 
+
+  return div1;
+
+}
+
+function food2() {
+  const div2 = document.createElement('div');
+  const Par2 = document.createElement('p');
+  Par2.textContent = `second food `;
+  Par2.classList.add(`foodText`)
+
+  const image2 = document.createElement('img');
+  image2.classList.add(`foodImg`)
+  image2.src = "img/img2.jpg"
+
+  div2.appendChild(image2)
+  div2.appendChild(Par2)
+ 
+
+  return div2;
+
+}
+
+function food3() {
+  const div3 = document.createElement('div');
+  const Par3 = document.createElement('p');
+  Par3.textContent = `this one is the third one ,,  `;
+  Par3.classList.add(`foodText`)
+
+  const image3 = document.createElement('img');
+  image3.classList.add(`foodImg`)
+  image3.src = "img/img3.jpg"
+
+  div3.appendChild(image3)
+  div3.appendChild(Par3)
+ 
+
+  return div3;
+
+}
+
+function food4() {
+  const div4 = document.createElement('div');
+  const Par4 = document.createElement('p');
+  Par4.textContent = `the last one ,,  `;
+  Par4.classList.add(`foodText`)
+
+  const image4 = document.createElement('img');
+  image4.classList.add(`foodImg`)
+  image4.src = "img/img4.png"
+
+  div4.appendChild(image4)
+  div4.appendChild(Par4)
+ 
+
+  return div4;
+
+}
+
+
 
   //here it selecets the "Body" part to put the elemnts in it
 const bodyDiv = document.getElementById('body')
