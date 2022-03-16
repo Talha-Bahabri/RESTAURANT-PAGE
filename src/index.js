40216713630cd467b027bc5de734b3cf168d8c0b
Talha <talha.b.future@gmail.com>
2022-03-16 11:58:57 +0300

    quick changes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
 import _ from 'lodash';
 import  menu  from './modules/menu/food'
 
- 
-//   //here it selecets the "Body" part to put the elemnts in it
-// const bodyDiv = document.getElementById('body')
-// bodyDiv.appendChild(menu())
- 
+    //here it selecets the "Body" part to put the elemnts in it
+const bodyDiv = document.getElementById('body')
+
 
 
 const aboutButton = document.getElementById('about');
 aboutButton.addEventListener("click" , function(e) {
 
-    //here it selecets the "Body" part to put the elemnts in it
-const bodyDiv = document.getElementById('body')
-bodyDiv.appendChild(menu());
+  console.log('about presssssessss')
+
+// bodyDiv.appendChild(about());
 
 });
+
+const menuButton = document.getElementById('menu');
+menuButton.addEventListener("click" , function(e) {
+
+ bodyDiv.appendChild(menu());
+
+});
+ 
+ 
+function about () {
+  const mainDiv = document.createElement('div');
+  mainDiv.classList.add('aboutDiv')
+
+  const aboutPara = document.createElement('p')
+  
+
+
+ }
