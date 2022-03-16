ab27915ab65bbdb6a03efad673dfd12428e8de62
Talha <talha.b.future@gmail.com>
2022-03-16 18:04:10 +0300

    about file works fine now, might need add animtion

--- src/modules/menu/about.js
+++ src/modules/menu/about.js
@@ -2,6 +2,17 @@
 export default function about () {
     const mainDiv = document.createElement('div');
     mainDiv.classList.add('aboutDiv')
+
+     //Combine them in the main div
+     mainDiv.appendChild(aboutText());
+     mainDiv.appendChild(aboutImgs());
+   
+    return mainDiv;
+  
+   }
+
+   function aboutText() {
+
   
     const aboutDivTextDiv = document.createElement(`div`);
     aboutDivTextDiv.classList.add(`aboutDivTextDiv`);
@@ -15,6 +26,12 @@ export default function about () {
      //After adding all what we need in the paragraph, we appeand it to the FIRST div
      aboutDivTextDiv.appendChild(aboutPara);
   
+     return aboutDivTextDiv;
+   }
+
+
+   function aboutImgs() {
+       
      const aboutDivImgDiv = document.createElement(`div`);
      aboutDivImgDiv.classList.add(`aboutDivImgDiv`);
   
@@ -36,11 +53,6 @@ export default function about () {
      aboutDivImgDiv.appendChild(img1);
      aboutDivImgDiv.appendChild(img2);
      aboutDivImgDiv.appendChild(img3);
-  
-     //Combine them in the main div
-     mainDiv.appendChild(aboutDivTextDiv);
-     mainDiv.appendChild(aboutDivImgDiv);
-   
-    return mainDiv;
-  
+
+     return aboutDivImgDiv;
    }
