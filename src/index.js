be149a17ad340f3c6104923f04d84df3003e679e
Talha <talha.b.future@gmail.com>
2022-03-16 11:31:41 +0300

    put it under the button the menu

--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
 import _ from 'lodash';
-import  menu  from './modules/menu/food' 
+import  menu  from './modules/menu/food'
 
- function component() {
-   const element = document.createElement('div');
-
-  //  Lodash, now imported by this script
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  
-
-  console.log(`444444444444444444444444444`)
-
-   return element;
- }
- document.body.appendChild(component());                    
-   
-
+ 
   //here it selecets the "Body" part to put the elemnts in it
 const bodyDiv = document.getElementById('body')
 bodyDiv.appendChild(menu())
@@ -24,5 +10,9 @@ bodyDiv.appendChild(menu())
 
 const aboutButton = document.getElementById('about');
 aboutButton.addEventListener("click" , function(e) {
-  console.log(`about is pressed`);
+
+    //here it selecets the "Body" part to put the elemnts in it
+const bodyDiv = document.getElementById('body')
+bodyDiv.appendChild(menu());
+
 });
